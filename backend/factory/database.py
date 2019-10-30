4bf059751f6c95804d8a8a6876578c612afe757f
Al Mamun Khan <almamunkhan04@gmail.com>
2019-10-31 01:20:35 +0600

    fixed async db connection , insert and delete method added

--- backend/factory/database.py
+++ backend/factory/database.py
@@ -1,7 +1,17 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import config
 
 class Database(object):
     def __init__(self):
-        self.client = MongoClient(config["db"]["url"])  #configure db
+        self.client = AsyncIOMotorClient(config["db"]["url"])  #configure db
         self.db     = self.client[config["db"]["name"]]
+
+    async def inset(self, element, collection_name):
+        element["created"] = datetime.now()
+        element["updated"] = datetime.now()
+        inserted = await self.db[collection_name].insert_one(element)  # insert data to db
+        return str(inserted.inserted_id)
+    
+    async def delete(self, id, collection_name):
+        deleted = await self.db[collection_name].delete_one({"_id": ObjectId(id)})
+        return bool(deleted.deleted_count)
