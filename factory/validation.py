759d2e500d7820a491e17964c92bef55258608de
Al Mamun Khan <almamunkhan04@gmail.com>
2019-11-09 18:07:54 +0600

    Update validation.py

--- factory/validation.py
+++ factory/validation.py
@@ -14,3 +14,24 @@ class Validator(object):
         if type(desired_type) == list:
             return (element in desired_type)
         raise ValueError("Invalid value for desired type")
+     def validateTypes(self, element, fields):
+        for field in fields:
+            if field in element:
+                if not self.validate_type(element[field], fields[field]):
+                    return False
+            return True
+
+    def validate(self, element, fields, required_fields, optional_fields):
+        if not self.validateTypes(element, fields):
+            raise ValueError("Invalid type of field")
+
+        element_fields  = set(element.keys())
+        required_fields = set(required_fields)
+        optional_fields = set(optional_fields)
+
+
+        if len(required_fields - element_fields) > 0:
+            raise ValueError("Required field missing")
+
+        if len(element_fields - (required_fields | optional_fields)) > 0:
+            raise ValueError("Invalid field in element")
